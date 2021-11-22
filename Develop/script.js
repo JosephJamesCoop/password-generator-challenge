bfd2401c8e6417e5dc029b0825a2c6e03902dff4
JosephJamesCoop <91605342+JosephJamesCoop@users.noreply.github.com>
2021-11-21 19:26:53 -0800

    maximum charcter length

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,19 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   sessionStorage.clear()
@@ -10,31 +23,42 @@ function writePassword() {
     switch (minimumLength) {
       case "4":
         sessionStorage.setItem("min", "4")
-        // maximumPasswordLength();
       case "8":
         sessionStorage.setItem("min", "8")
-        // maximumPasswordLength();
       case "12":
         sessionStorage.setItem("min", "12")
-        // maximumPasswordLength();
       case "16":
         sessionStorage.setItem("min", "16")
-        // maximumPasswordLength();
       case "20":
         sessionStorage.setItem("min", "20")
-        // maximumPasswordLength();
         break;
       default:
         window.alert("Please type a numeric value that corresponds with the listed minimum number of characters you would like. For example: Enter '4' to select 4 characters");
         writePassword();
         break;
     }
+    var maximumLength = window.prompt("Type in a MAXIMUM character length for your password; 40, 60, 80, 100, or 128"
+  );
+  switch (maximumLength) {
+    case "40":
+      sessionStorage.setItem("max", "40")
+    case "60":
+      sessionStorage.setItem("max", "60")
+    case "80":
+      sessionStorage.setItem("max", "80")
+    case "100":
+      sessionStorage.setItem("max", "100")
+    case "128":
+      sessionStorage.setItem("max", "128")
+    default:
+      sessionStorage.setItem("max", "128")
+      break;
+  }
 
 
 
 
 
-    
 function generatePassword(){
   return "40"
 };
