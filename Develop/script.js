5033709899802142e471623de3bf01b58acb53d1
JosephJamesCoop <91605342+JosephJamesCoop@users.noreply.github.com>
2021-11-21 21:55:03 -0800

    compressed min and max to number select

--- Develop/script.js
+++ Develop/script.js
@@ -2,58 +2,21 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Write password to the #password input
 function writePassword() {
+  // clears saved data
   sessionStorage.clear()
-    var minimumLength = window.prompt("type in a MINIMUM character length for your password; 4, 8, 12, 16, or 20"
-    );
-    switch (minimumLength) {
-      case "4":
-        sessionStorage.setItem("min", "4")
-      case "8":
-        sessionStorage.setItem("min", "8")
-      case "12":
-        sessionStorage.setItem("min", "12")
-      case "16":
-        sessionStorage.setItem("min", "16")
-      case "20":
-        sessionStorage.setItem("min", "20")
-        break;
-      default:
-        window.alert("Please type a numeric value that corresponds with the listed minimum number of characters you would like. For example: Enter '4' to select 4 characters");
-        writePassword();
-        break;
+  // select minimum length
+  var minimumLength = window.prompt("please select a password length between 8 and 128 characters.");
+ if (minimumLength >= 8 && minimumLength < 129 ) {
+    var pickedLength = Math.floor(minimumLength);
+      sessionStorage.setItem("min", pickedLength)
+ }
+    else {
+      window.alert("Please type a numeric value that corresponds with the number of characters you would like. For example: Enter '34' to select 34 characters");
+      writePassword();
     }
-    var maximumLength = window.prompt("Type in a MAXIMUM character length for your password; 40, 60, 80, 100, or 128"
-  );
-  switch (maximumLength) {
-    case "40":
-      sessionStorage.setItem("max", "40")
-    case "60":
-      sessionStorage.setItem("max", "60")
-    case "80":
-      sessionStorage.setItem("max", "80")
-    case "100":
-      sessionStorage.setItem("max", "100")
-    case "128":
-      sessionStorage.setItem("max", "128")
-    default:
-      sessionStorage.setItem("max", "128")
-      break;
-  }
+  // do you want lowercase?
   var lowercase = window.confirm("Would you like to include LOWERCASE letters?");
   if (lowercase) {
     sessionStorage.setItem("1", "abcdefghijklmnopqrstuvwxyz")
@@ -61,6 +24,7 @@ function writePassword() {
   else {
     sessionStorage.setItem("1", "")
   }
+  //do you want uppercase
     var uppercase = window.confirm("Would you like to include UPPERCASE letters?");
     if (uppercase) {
       sessionStorage.setItem("2", "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
@@ -68,6 +32,7 @@ function writePassword() {
     else {
       sessionStorage.setItem("2", "");
     }
+    // do you want numeric values
     var numericValues = window.confirm("Would you like to include Numeric Values?");
     if (numericValues) {
       sessionStorage.setItem("3", "0123456789");
@@ -75,6 +40,7 @@ function writePassword() {
     else {
       sessionStorage.setItem("3", "")
     }
+    // do you want special characters
     var specialCharacters = window.confirm("Would you like to include Special Characters?");
     if (specialCharacters) {
       sessionStorage.setItem("4", "!@#$%^&*()");
@@ -84,8 +50,33 @@ function writePassword() {
     }
 
 
-
+//generates results
 function generatePassword(){
+  var upper = sessionStorage.getItem("1");
+  var lower = sessionStorage.getItem("2");
+  var numeric = sessionStorage.getItem("3");
+  var special = sessionStorage.getItem("4");
+  var minimum = sessionStorage.getItem("min");
+  var maximum = sessionStorage.getItem("max");
+  var choice = upper + lower + numeric + special;
+
+  var value = Math.floor(Math.random() * (maximum - minimum) + minimum + 1);
+
+const str = choice;
+
+function sumAll() {
+        let sum = 0;
+        for (let i = 0; i < value; i++) {
+        firstString += str.substr(1, 2)[i];
+      }
+
+
+console.log(str.substr(1, 2));
+
+  
+
+
+
   return "40"
 };
 
