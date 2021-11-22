41446474c68f3277f5b805fcd334f3edc94649e8
JosephJamesCoop <91605342+JosephJamesCoop@users.noreply.github.com>
2021-11-22 10:08:03 -0800

    guaratees one of each character type selected is added

--- Develop/script.js
+++ Develop/script.js
@@ -8,77 +8,149 @@ function writePassword() {
   sessionStorage.clear()
   // select minimum length
   var minimumLength = window.prompt("please select a password length between 8 and 128 characters.");
- if (minimumLength >= 8 && minimumLength < 129 ) {
+  if (minimumLength >= 8 && minimumLength < 129) {
     var pickedLength = Math.floor(minimumLength);
-      sessionStorage.setItem("min", pickedLength)
- }
-    else {
-      window.alert("Please type a numeric value that corresponds with the number of characters you would like. For example: Enter '34' to select 34 characters");
-      writePassword();
-    }
+    sessionStorage.setItem("min", pickedLength)
+  }
+  else {
+    window.alert("Please type a numeric value that corresponds with the number of characters you would like. For example: Enter '34' to select 34 characters");
+    writePassword();
+  }
   // do you want lowercase?
   var lowercase = window.confirm("Would you like to include LOWERCASE letters?");
   if (lowercase) {
-    sessionStorage.setItem("1", "abcdefghijklmnopqrstuvwxyz")
+    var one = "abcdefghijklmnopqrstuvwxyz"
+    var lower = one.split("").sort(
+      function () {
+        return 0.5 - Math.random()
+      }).join("");
+    sessionStorage.setItem("1", lower)
+    var oneOne = lower[4]
   }
   else {
     sessionStorage.setItem("1", "")
+    var oneOne = ""
   }
   //do you want uppercase
-    var uppercase = window.confirm("Would you like to include UPPERCASE letters?");
-    if (uppercase) {
-      sessionStorage.setItem("2", "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    }
-    else {
-      sessionStorage.setItem("2", "");
-    }
-    // do you want numeric values
-    var numericValues = window.confirm("Would you like to include Numeric Values?");
-    if (numericValues) {
-      sessionStorage.setItem("3", "0123456789");
-    }
-    else {
-      sessionStorage.setItem("3", "")
-    }
-    // do you want special characters
-    var specialCharacters = window.confirm("Would you like to include Special Characters?");
-    if (specialCharacters) {
-      sessionStorage.setItem("4", "!@#$%^&*()");
-    }
-    else {
-      sessionStorage.setItem("4", "");  
+  var uppercase = window.confirm("Would you like to include UPPERCASE letters?");
+  if (uppercase) {
+var two = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var upper = two.split("").sort(
+      function () {
+        return 0.5 - Math.random()
+      }).join("");
+    sessionStorage.setItem("2", upper);
+      var twoOne = upper[4]
+  }
+  else {
+    sessionStorage.setItem("2", "");
+    var twoOne = ""
+  }
+  // do you want numeric values
+  var numericValues = window.confirm("Would you like to include Numeric Values?");
+  if (numericValues) {
+var three = "0123456789"
+    var numeric = three.split("").sort(
+      function () {
+        return 0.5 - Math.random()
+      }).join("");
+    sessionStorage.setItem("3", numeric);
+    var threeOne = numeric[4]
+  }
+  else {
+    sessionStorage.setItem("3", "")
+    var threeOne =""
+  }
+  // do you want special characters
+  var specialCharacters = window.confirm("Would you like to include Special Characters?");
+  if (specialCharacters) {
+var four = "!@#$%^&*()"
+    var special = four.split("").sort(
+      function () {
+        return 0.5 - Math.random()
+      }).join("");
+    sessionStorage.setItem("4", special);
+      var fourOne = special[4]
+  }
+  else {
+    sessionStorage.setItem("4", "");
+    var fourOne = ""
+  }
+
+
+  //generates results
+  function generatePassword() {
+    var first = sessionStorage.getItem("1");
+    var second = sessionStorage.getItem("2");
+    var third = sessionStorage.getItem("3");
+    var fourth = sessionStorage.getItem("4");
+    var value = sessionStorage.getItem("min");
+
+    
+    // first = ("" || null)
+    // if (first)
+    // console.log()
+      
+    //   lower[3]
+    // var second = upper[3]
+    // var third = numeric[3]
+    // var fourth = special[3]
+
+
+    console.log(fourOne) 
+
+    console.log(third) 
+    console.log(fourth) 
+
+    var quantity = oneOne + twoOne + threeOne + fourOne;
+
+    console.log(quantity)
+
+    var choice = first + second + third + fourth;
+    console.log(choice) 
+
+    
+    var length = value - 1 - quantity.length
+
+    var longChoice = choice + choice + choice + choice + choice + choice + choice + choice + choice + choice + choice + choice + choice;
+
+    var mixOne = longChoice.split("").sort(
+      function () {
+        return 0.5 - Math.random()
+      }).join("");
+
+    var mixTwo = mixOne.split("").sort(
+      function () {
+        return 0.5 - Math.random()
+      }).join("");
+
+    var mixThree = mixTwo.split("").sort(
+      function () {
+        return 0.5 - Math.random()
+      }).join("");
+
+    let radnom = mixThree[66];
+    for (let i = 0; i < length; i++) {
+      var mixed = radnom += mixThree.substr()[i];
     }
 
 
-//generates results
-function generatePassword(){
-  var upper = sessionStorage.getItem("1");
-  var lower = sessionStorage.getItem("2");
-  var numeric = sessionStorage.getItem("3");
-  var special = sessionStorage.getItem("4");
-  var value = sessionStorage.getItem("min");
-  var choice = upper + lower + numeric + special;
-  var length = value - 1
-var longChoice = choice + choice + choice + choice + choice + choice + choice + choice + choice + choice + choice + choice + choice;
 
-var mixOne = longChoice.split("").sort(
-  function(){return 0.5-Math.random()
-  }).join("");
+    var shuffle = mixed + first + second + third + fourth;
 
-var mixTwo = mixOne.split("").sort(
-  function(){return 0.5-Math.random()
-  }).join("");
 
-var mixThree = mixTwo.split("").sort(
-  function(){return 0.5-Math.random()
-  }).join("");
 
-let seventh = mixThree[6];
-  for (let i = 0; i < length; i++) {
-  var answer = seventh += mixThree.substr()[i];
-      }
-  return answer
-};
+
+
+
+    var answer = shuffle.split("").sort(
+      function () {
+        return 0.5 - Math.random()
+      }).join("");
+
+
+    return answer
+  };
 
 
 
@@ -88,7 +160,7 @@ let seventh = mixThree[6];
   passwordText.value = password;
 
 
-console.log("hello");
+
 };
 
 // Add event listener to generate button
