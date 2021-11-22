39d401f2341572a7525ef6f8306d534d2e86438b
JosephJamesCoop <91605342+JosephJamesCoop@users.noreply.github.com>
2021-11-22 10:24:07 -0800

    remmoved session storage to clean up code

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,12 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
-  // clears saved data
-  sessionStorage.clear()
   // select minimum length
   var minimumLength = window.prompt("please select a password length between 8 and 128 characters.");
   if (minimumLength >= 8 && minimumLength < 129) {
-    var pickedLength = Math.floor(minimumLength);
-    sessionStorage.setItem("min", pickedLength)
+    var value = Math.floor(minimumLength);
   }
   else {
     window.alert("Please type a numeric value that corresponds with the number of characters you would like. For example: Enter '34' to select 34 characters");
@@ -24,96 +20,61 @@ function writePassword() {
       function () {
         return 0.5 - Math.random()
       }).join("");
-    sessionStorage.setItem("1", lower)
     var oneOne = lower[4]
   }
   else {
-    sessionStorage.setItem("1", "")
     var oneOne = ""
   }
   //do you want uppercase
   var uppercase = window.confirm("Would you like to include UPPERCASE letters?");
   if (uppercase) {
-var two = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var two = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     var upper = two.split("").sort(
       function () {
         return 0.5 - Math.random()
       }).join("");
-    sessionStorage.setItem("2", upper);
-      var twoOne = upper[4]
+    var twoOne = upper[4]
   }
   else {
-    sessionStorage.setItem("2", "");
     var twoOne = ""
   }
   // do you want numeric values
   var numericValues = window.confirm("Would you like to include Numeric Values?");
   if (numericValues) {
-var three = "0123456789"
+    var three = "0123456789"
     var numeric = three.split("").sort(
       function () {
         return 0.5 - Math.random()
       }).join("");
-    sessionStorage.setItem("3", numeric);
     var threeOne = numeric[4]
   }
   else {
-    sessionStorage.setItem("3", "")
-    var threeOne =""
+    var threeOne = ""
   }
   // do you want special characters
   var specialCharacters = window.confirm("Would you like to include Special Characters?");
   if (specialCharacters) {
-var four = "!@#$%^&*()"
+    var four = "!@#$%^&*()"
     var special = four.split("").sort(
       function () {
         return 0.5 - Math.random()
       }).join("");
-    sessionStorage.setItem("4", special);
-      var fourOne = special[4]
+    var fourOne = special[4]
   }
   else {
-    sessionStorage.setItem("4", "");
     var fourOne = ""
   }
-
-
   //generates results
   function generatePassword() {
-    var first = sessionStorage.getItem("1");
-    var second = sessionStorage.getItem("2");
-    var third = sessionStorage.getItem("3");
-    var fourth = sessionStorage.getItem("4");
-    var value = sessionStorage.getItem("min");
-
-    
-    // first = ("" || null)
-    // if (first)
-    // console.log()
-      
-    //   lower[3]
-    // var second = upper[3]
-    // var third = numeric[3]
-    // var fourth = special[3]
-
-
-    console.log(fourOne) 
-
-    console.log(third) 
-    console.log(fourth) 
-
-    var quantity = oneOne + twoOne + threeOne + fourOne;
-
-    console.log(quantity)
-
-    var choice = first + second + third + fourth;
-    console.log(choice) 
-
-    
+    //collects one character from each item slected to guarantee one of each
+    var quantity = oneOne + twoOne + threeOne + fourOne
+    // combines all charcters in charter selected option
+    var choice = lower + upper + numeric + special;
+    //selects number of charcters slected then adds all additional till number chosen is reached.
     var length = value - 1 - quantity.length
-
+    // guartees you can have 128 charcters in your passcode. Also allows for individual charcters to be used multiple times.
     var longChoice = choice + choice + choice + choice + choice + choice + choice + choice + choice + choice + choice + choice + choice;
-
+    //shuffles all selectsions 3 times
     var mixOne = longChoice.split("").sort(
       function () {
         return 0.5 - Math.random()
@@ -128,40 +89,24 @@ var four = "!@#$%^&*()"
       function () {
         return 0.5 - Math.random()
       }).join("");
-
-    let radnom = mixThree[66];
+    //selects quantity selected - however many choices were made
+    let random = mixThree[66];
     for (let i = 0; i < length; i++) {
-      var mixed = radnom += mixThree.substr()[i];
+      var mixed = random += mixThree.substr()[i];
     }
-
-
-
-    var shuffle = mixed + first + second + third + fourth;
-
-
-
-
-
-
+    //generates one of each selction plus random then mixes again
+    var shuffle = mixed + quantity;
     var answer = shuffle.split("").sort(
       function () {
         return 0.5 - Math.random()
       }).join("");
-
-
+    // returns the password with the specified qualifications
     return answer
   };
-
-
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-
-
 };
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
