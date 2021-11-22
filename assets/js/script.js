ee31dadacba9d1862cdb1317f0513e1b7a391aa3
JosephJamesCoop <91605342+JosephJamesCoop@users.noreply.github.com>
2021-11-22 11:51:56 -0800

    added verifcation statement about selected charcters

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,9 +21,12 @@ function writePassword() {
         return 0.5 - Math.random()
       }).join("");
     var oneOne = lower[4]
+    var lowerValue = "It will include Lowercase Charcters. "
   }
   else {
+    var lower = ""
     var oneOne = ""
+    var lowerValue = "It will NOT include Lowercase Charcters. "
   }
   //do you want uppercase
   var uppercase = window.confirm("Would you like to include UPPERCASE letters?");
@@ -34,9 +37,12 @@ function writePassword() {
         return 0.5 - Math.random()
       }).join("");
     var twoOne = upper[4]
+    var upperValue = "It will include Uppercase Charcters. "
   }
   else {
+    var upper = ""
     var twoOne = ""
+    var upperValue = "It will NOT include Uppercase Charcters. "
   }
   // do you want numeric values
   var numericValues = window.confirm("Would you like to include Numeric Values?");
@@ -47,9 +53,12 @@ function writePassword() {
         return 0.5 - Math.random()
       }).join("");
     var threeOne = numeric[4]
+    var numericValue = "It will include Numeric Values. "
   }
   else {
+    var numeric = ""
     var threeOne = ""
+    var numericValue = "It will NOT include Numeric Values. "
   }
   // do you want special characters
   var specialCharacters = window.confirm("Would you like to include Special Characters?");
@@ -60,16 +69,33 @@ function writePassword() {
         return 0.5 - Math.random()
       }).join("");
     var fourOne = special[4]
+    var specialValue = "It will include Special Charcters. "
   }
   else {
     var fourOne = ""
+    var special = ""
+    var specialValue = "It will NOT include Special Charcters. "
+  };
+    // combines all charcters in charter selected option
+    var choice = lower + upper + numeric + special;
+    if (choice.length > 1) {
+      var confirmSelection =window.confirm("Your password will be " + minimumLength + " charcters long. " + lowerValue + upperValue + numericValue + specialValue + "Does this look correct?");
+  if (confirmSelection) {
+    window.alert("Thank you for your confirmation. Here is your password!")
   }
+  else {
+    writePassword();
+  };
+    }
+    else {
+      window.alert("Please select at least one option")
+      writePassword();
+    };
   //generates results
   function generatePassword() {
     //collects one character from each item slected to guarantee one of each
     var quantity = oneOne + twoOne + threeOne + fourOne
-    // combines all charcters in charter selected option
-    var choice = lower + upper + numeric + special;
+
     //selects number of charcters slected then adds all additional till number chosen is reached.
     var length = value - 1 - quantity.length
     // guartees you can have 128 charcters in your passcode. Also allows for individual charcters to be used multiple times.
